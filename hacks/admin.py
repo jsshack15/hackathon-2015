12e15eaf484b9a406165e0833e3da2e9f9e42392
deshraj <deshrajdry@gmail.com>
2015-11-08 15:06:04 +0530

    Admin list_display added

--- hacks/admin.py
+++ hacks/admin.py
@@ -2,5 +2,13 @@ from django.contrib import admin
 from hacks.models import Hackathon, CodeMania
 # Register your models here.
 
-admin.site.register(Hackathon)
-admin.site.register(CodeMania)
+
+class HackathonAdmin(admin.ModelAdmin):
+	list_display = ("name", "email", "phone_number", "github", "linkedin", "mac_address", "hardware_required")
+
+
+class CodeManiaAdmin(admin.ModelAdmin):
+	list_display = ("name", "email", "phone_number", "course", "branch", "year")
+
+admin.site.register(Hackathon, HackathonAdmin)
+admin.site.register(CodeMania, CodeManiaAdmin)
